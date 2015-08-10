9ed417f81d3668ed918d4444a0b278830fbbe689
Michel Valin <mfvalin@gmail.com>
2015-08-10 12:29:04 -0400

    modified debugging options

--- run_sps.sh
+++ run_sps.sh
@@ -2,10 +2,10 @@
 #
 while [[ $# -gt 0 ]] ; do
    case $1 in
-      (-v|--verbose)     VeRbOsE="-x"   ;;   
-      (-d|--debug)       SPS_DEBUG="yes" ;;
-      (-gdb|--gdb)       export RUN_IN_PARALLEL_EXTRAS="${RUN_IN_PARALLEL_EXTRAS} -preexec gdb" ;;
-      (-preexec|--preexec)  export RUN_IN_PARALLEL_EXTRAS="${RUN_IN_PARALLEL_EXTRAS} -preexec ${2:-gdb}" ; shift ;;
+      (-v|--verbose)        VeRbOsE="-x"   ;;   
+      (-d|--debug)          SPS_DEBUG="yes" ;;
+      (-gdb|--gdb)          export RUN_IN_PARALLEL_EXTRAS="${RUN_IN_PARALLEL_EXTRAS} -preexec gdb"       ; SPS_DEBUG="yes" ;;
+      (-preexec|--preexec)  export RUN_IN_PARALLEL_EXTRAS="${RUN_IN_PARALLEL_EXTRAS} -preexec ${2:-gdb}" ; SPS_DEBUG="yes" ; shift ;;
       (-*) echo "Error: unknown option '$1'"; exit 1;;
    esac
    previous=$1
