1d177ed5c75458c42abf66f314da068ef1239653
Michel Valin <valin.michel@sca.uqam.ca>
2015-11-18 09:14:15 -0500

    fleshed out level filler

--- fill_levels.F90
+++ fill_levels.F90
@@ -6,31 +6,44 @@ program fill_levels
   integer :: levels, nvars
   character(len=4096) :: filename
   character(len=4), dimension(MAX_NAMES) :: names
-  integer :: i, j, status, length, arb_code, iun, key
+  integer :: i, j, status, length, arb_code, iun, key, lastread, errors
   character(len=16) :: string
   integer, dimension(:), allocatable :: codes
   real*4, dimension(:), pointer :: z
   real*4 :: temp
-  integer :: ni, nj, nk, nrec
+  real*8 :: hours
+  integer :: ni, nj, nk, nrec, ni0, nj0, nk0, arg0
   integer :: nbits,datyp,ip1,ip2,ip3,dateo,deet,npas
-  integer :: datev, new_datev
+  integer :: datev
   integer :: ig1,ig2,ig3,ig4,swa,lng,dltf,ubc,extra1,extra2,extra3
   character(len=1) :: grtyp
   character(len=4) :: nomvar
   character(len=2) :: typvar
   character(len=12) :: etiket
-  integer, external :: fstouv, fnom, fstinf, fstluk
+  integer, external :: fstouv, fnom, fstinf, fstluk, fstlir
+  logical :: verify_only
 
   filename = 'DoesNotExistYet'
+  verify_only = .false.
   names = "    "
   levels = 0
+  errors = 0
   allocate(z(1000000))
 
-  CALL GET_COMMAND_ARGUMENT(1 , filename, length, status)
+  arg0 = 0
+  CALL GET_COMMAND_ARGUMENT(arg0 + 1 , filename, length, status)
+  if(status .ne. 0) goto 999
+  if(trim(filename) == '--check') then
+    verify_only = .true.
+    print 100,"INFO: VERIFY ONLY"
+    arg0 = arg0 + 1
+  endif
+
+  CALL GET_COMMAND_ARGUMENT(arg0 + 1 , filename, length, status)
   if(status .ne. 0) goto 999
   print 100,"INFO: processing '"//trim(filename)//"'"
 
-  CALL GET_COMMAND_ARGUMENT(2 , string, length, status)
+  CALL GET_COMMAND_ARGUMENT(arg0 + 2 , string, length, status)
   if(status .ne. 0) goto 999
 
   read(string,*,iostat=status) levels
@@ -45,7 +58,7 @@ program fill_levels
   enddo
   print *,'INFO: codelist=',codes
 
-  CALL GET_COMMAND_ARGUMENT(3 , string, length, status)
+  CALL GET_COMMAND_ARGUMENT(arg0 + 3 , string, length, status)
   if(status .ne. 0) goto 999
 
   read(string,*,iostat=status) nvars
@@ -53,7 +66,7 @@ program fill_levels
   print 100,"INFO: number of variables =",nvars
 
   do i = 1, nvars
-    CALL GET_COMMAND_ARGUMENT(i+3 , names(i), length, status)
+    CALL GET_COMMAND_ARGUMENT(arg0 + i + 3 , names(i), length, status)
     if(status .ne. 0) goto 999
     print 100,"INFO: variable",i," = '"//names(i)//"'"
   enddo
@@ -71,25 +84,57 @@ program fill_levels
   endif
 
   do i = 1, nvars
-    key = fstinf(iun,ni,nj,nk,-1,"",codes(1),-1,-1,"",names(i))  ! first level
+    nomvar = names(i)
+    key = fstinf(iun,ni,nj,nk,-1,"",codes(1),-1,-1,"",nomvar)  ! first level
     if(size(z) < ni*nj) then
       deallocate(z)
       allocate(z(ni*nj+10))
     endif
-    status = fstluk(z,key,ni,nj,nk)
+    if(.not. verify_only) then
+      status = fstluk(z,key,ni,nj,nk)
+    else
+        print 100,'INFO: found level',1,' of variable "'//nomvar//'"'
+    endif
     call fstprm(key,dateo,deet,npas,ni,nj,nk,nbits,datyp,ip1,ip2,ip3, &
                 typvar,nomvar,etiket,grtyp,ig1,ig2,ig3,ig4, &
                 swa,lng,dltf,ubc,extra1,extra2,extra3)
+    lastread = 1
     do j = 2, levels  ! try to read all levels, if a level is not found, write it
+      hours = deet * npas
+      hours = hours / 3600.0
+      call incdatr(datev,dateo,hours)
+      key = fstinf(iun,ni0,nj0,nk0,datev,etiket,codes(j),ip2,ip3,typvar,nomvar)
+      if(key <0) then
+        if(verify_only) then
+          print 100,'ERROR: level',j,' of variable "'//nomvar//'" is missing'
+          errors = errors + 1
+        else
+          print 100,'INFO: adding level',lastread,' of variable "'//nomvar//'" as level',j
+          call fstecr(z,z,-nbits,iun,dateo,deet,npas,ni,nj,nk,codes(j),ip2,ip3,typvar,nomvar,etiket,grtyp,ig1,ig2,ig3,ig4,datyp,.false.)
+        endif
+      else
+        if(.not. verify_only) then
+          status = fstluk(z,key,ni,nj,nk)
+          print 100,'INFO: keeping level',j,' of variable "'//nomvar//'"'
+        else
+          print 100,'INFO: found level',j,' of variable "'//nomvar//'"'
+        endif
+        lastread = j
+      endif
     enddo
   enddo
 
   call fstfrm(iun)
+  if(errors > 0) then
+    print 100,'===== ERRORS IN RUN ====='
+    call qqexit(1)
+  endif
 
 888 continue
   stop
-  100 format(A,I4,1X,A)
+  100 format(A,I4,1X,A,I4)
 999 continue
   print *,"ERROR in arguments, aborting run"
+  call qqexit(1)
   stop
 end
