7c2c59c808c1b2ba35116a4423d92cab31ca7d19
Michel Valin <valin.michel@sca.uqam.ca>
2016-03-22 11:04:46 -0400

    fixed signal trapping

--- run_sps.sh
+++ run_sps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-trap 'trap "" 0 1 2 3 4 5 6 7 8 10 12 13 14 15 ; echo "User requested abort" ; save_crash; cleanup_dirs ; exit 1' 0 1 2 3 4 5 6 7 8 10 12 13 14 15
+trap 'trap "" 0 1 2 3 4 5 6 7 8 10 12 13 14 15 ; echo "User requested abort" ; save_crash; cleanup_dirs ; exit 1' 2
 usage()
 {
 cat <<EOT
